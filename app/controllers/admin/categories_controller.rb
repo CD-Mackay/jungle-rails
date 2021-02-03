5684a63deb58433c04ed36f39157bde24a40e4f7
CD-Mackay <connor.mackay@gmail.com>
2021-02-03 23:41:42 +0000

    add methods to category admin controller

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,8 +1,22 @@
 class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with name: "Jungle", password: "book"
+
   def index
+    @categories = Category.order(id: :desc).all
   end
 
-  def show
+  def new
+    @category = Category.new
   end
-  
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categorys], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
 end
