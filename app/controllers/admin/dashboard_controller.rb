b4d7fc9564bb864c4b0b61acec552cf72dfac09f
CD-Mackay <connor.mackay@gmail.com>
2021-02-03 21:10:31 +0000

    generate product and category instance variables

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: "Jungle", password: "book"
   def show
+    @products = Product.find_by_sql("SELECT * FROM products")
+    @categories = Category.find_by_sql("SELECT * FROM categories")
   end
 end
