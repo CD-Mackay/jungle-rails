0477dbd20594ac38b6aceed0c6cddcea0ef001d9
CD-Mackay <connor.mackay@gmail.com>
2021-02-04 17:51:38 +0000

    debug routing for login/logout

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,7 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all.order(created_at: :desc)
+    puts session[:user_id]
   end
 
   def show
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,12 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
-      session[:user_id] = user.id
-      redirect_to '/'
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path
     else
-      redirect_to '/signup'
+      redirect_to new_user_path
     end
 
   end
@@ -16,6 +16,6 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
-  
+
 
 end
